42a5533b3327eca4df1c09b1c74e3b9afb90f258
xavierwwj <xavierwwj@hotmail.sg>
2017-09-27 00:43:50 +0800

    Added ball speed changes, fixed bugs

--- pong.py
+++ pong.py
@@ -12,6 +12,7 @@ game_pause = False
 clock = pygame.time.Clock() 
 FPS = 30
 playtime = 0.0
+roundtime = 0.0
 
 # Display constants
 screen_width = 640
@@ -51,22 +52,28 @@ ball_angle_rad = ball_angle/360.0*2.0*math.pi
 
 ball_x = 320.0
 ball_y = 240.0
-ball_speed = 1.0
-ball_xspeed = 300.0*math.cos(ball_angle_rad)*ball_speed # in pixels per sec
-ball_yspeed = -300.0*math.sin(ball_angle_rad)*ball_speed # in pixels per sec
-dx = ball_xspeed / 30.0
-dy = ball_yspeed / 30.0
+ball_speed = 1
+ball_xspeed = 200.0*math.cos(ball_angle_rad)*ball_speed # in pixels per sec
+ball_yspeed = -200.0*math.sin(ball_angle_rad)*ball_speed # in pixels per sec
+base_dx = ball_xspeed / 30.0 # divide by fps
+base_dy = ball_yspeed / 30.0
+dx = base_dx
+dy = base_dy
+speed_multi = 1
+ball_dir_x = 1
+ball_dir_y = 1
 
 # Paddle variables
 paddle_width = 4 # These are half widths/heights
 paddle_height = 30
 paddle_speed  = 15
+spacing = 8
 
-A_pos = {'x': paddle_width + paddle_height, 'y': 240} # center coordinate
+A_pos = {'x': paddle_width + spacing, 'y': 240} # center coordinate
 A_color = (255,0,0)
 A_rect = pygame.Rect(A_pos['x']-paddle_width, A_pos['y']-paddle_height,2*paddle_width, 2*paddle_height)
 
-B_pos = {'x': 640 - paddle_width - paddle_height, 'y': 240}
+B_pos = {'x': 640 - paddle_width - spacing, 'y': 240}
 B_color = (0,0,255)
 B_rect = pygame.Rect(B_pos['x']-paddle_width, B_pos['y']-paddle_height,2*paddle_width, 2*paddle_height)
 
@@ -74,8 +81,8 @@ B_rect = pygame.Rect(B_pos['x']-paddle_width, B_pos['y']-paddle_height,2*paddle_
 def reset_ball(angle, speed):
     x = 320.0
     y = 240.0
-    xspeed = 300.0*math.cos(angle)*speed # in pixels per sec
-    yspeed = -300.0*math.sin(angle)*speed # in pixels per sec
+    xspeed = 200.0*math.cos(angle)*speed # in pixels per sec
+    yspeed = -200.0*math.sin(angle)*speed # in pixels per sec
     dx = xspeed / 30.0
     dy = yspeed / 30.0
     return x,y,xspeed,yspeed,dx,dy
@@ -86,6 +93,12 @@ while running:
     # --UPDATE TIME--
     milliseconds = clock.tick(FPS)
     playtime += milliseconds / 1000.0
+    roundtime += milliseconds / 1000.0
+
+    # --UPDATE SPEED--
+    speed_multi = 1.0 + 0.5*(roundtime/15)
+    dx = base_dx*speed_multi*ball_dir_x
+    dy = base_dy*speed_multi*ball_dir_y
 
     # --PAUSE GAME--
     pause_seconds = 0
@@ -107,6 +120,7 @@ while running:
             pause_surface = score_font.render("", False, font_color)
             score_surface_a = score_font.render(str(a_score), False, font_color)
             score_surface_b = score_font.render(str(b_score), False, font_color)
+            roundtime = 0.0
 
     # --QUIT EVENT CONDITIONS--
     for event in pygame.event.get():
@@ -142,36 +156,36 @@ while running:
     ball_x += dx
     ball_y += dy
     if ball_x + ball_radius >= screen_width: # ball leaves area -> update/reset
-        ball_x, ball_y, ball_xspeed, ball_yspeed, dx, dy = reset_ball(random.choice(angle_seq)/360.0*2.0*math.pi, ball_speed)
-        A_pos = {'x': paddle_width + paddle_height, 'y': 240}
-        B_pos = {'x': 640 - paddle_width - paddle_height, 'y': 240}
+        ball_x, ball_y, ball_xspeed, ball_yspeed, base_dx, base_dy = reset_ball(random.choice(angle_seq)/360.0*2.0*math.pi, ball_speed)
+        A_pos = {'x': paddle_width + spacing, 'y': 240}
+        B_pos = {'x': 640 - paddle_width - spacing, 'y': 240}
         a_score += 1
         score_surface_a = score_font.render("", False, font_color)
         score_surface_b = score_font.render("", False, font_color)
         game_pause = True
     elif ball_x - ball_radius <= 0:
-        ball_x, ball_y, ball_xspeed, ball_yspeed, dx, dy = reset_ball(random.choice(angle_seq)/360.0*2.0*math.pi, ball_speed)        
+        ball_x, ball_y, ball_xspeed, ball_yspeed, base_dx, base_dy = reset_ball(random.choice(angle_seq)/360.0*2.0*math.pi, ball_speed)        
         b_score += 1
-        A_pos = {'x': paddle_width + paddle_height, 'y': 240}
-        B_pos = {'x': 640 - paddle_width - paddle_height, 'y': 240}
+        A_pos = {'x': paddle_width + spacing, 'y': 240}
+        B_pos = {'x': 640 - paddle_width - spacing, 'y': 240}
         score_surface_a = score_font.render("", False, font_color)
         score_surface_b = score_font.render("", False, font_color)
         game_pause = True
 
     elif ball_y + ball_radius >= screen_length: # ball hit wall -> bounces
         ball_y = screen_length - ball_radius
-        dy *= -1
+        ball_dir_y *= -1
     elif ball_y - ball_radius <= 0:
         ball_y = ball_radius
-        dy *= -1
+        ball_dir_y *= -1
 
     # ball hits paddle -> reflect
-    elif ball_x - ball_radius <= A_pos['x']+paddle_width and ball_x-ball_radius >= A_pos['x']-paddle_width and ball_y >= A_pos['y']-paddle_height and ball_y <= A_pos['y']+paddle_height:
+    elif ball_x - ball_radius <= A_pos['x']+paddle_width and ball_y >= A_pos['y']-1.1*paddle_height and ball_y <= A_pos['y']+1.1*paddle_height:
         ball_x = A_pos['x'] + paddle_width
-        dx *= -1
-    elif ball_x + ball_radius >= B_pos['x']-paddle_width and ball_x+ball_radius <= B_pos['x']+paddle_width and ball_y >= B_pos['y']-paddle_height and ball_y <= B_pos['y']+paddle_height:
+        ball_dir_x *= -1
+    elif ball_x + ball_radius >= B_pos['x']-paddle_width and ball_y >= B_pos['y']-1.1*paddle_height and ball_y <= B_pos['y']+1.1*paddle_height:
         ball_x = B_pos['x']-paddle_width
-        dx *= -1
+        ball_dir_x *= -1
     
     # --Drawing, refreshing events--
     screen.blit(background, (0,0))
